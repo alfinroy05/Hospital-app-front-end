f19505dd5d9636bee2445a108d879a72c29e2534
alfinroy05 <alfinroy2005@gmail.com>
2024-04-09 21:49:36 +0530

    complted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import DeleteHospital from './Hospital/DeleteHospital';
+import Index from './Hospital/Index';
+import SearchHospital from './Hospital/SearchHospital';
+import View from './Hospital/View';
+import AddPatient from './Hospital/AddPatient';
+import { BrowserRouter,Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Index/>}/>
+      <Route path='/add' element={<AddPatient/>}/>
+      <Route path='/delete' element={<DeleteHospital/>}/>
+      <Route path='/search' element={<SearchHospital/>}/>
+      <Route path='/view' element={<View/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
